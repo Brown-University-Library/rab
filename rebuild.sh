cf9965caa257dd8e5bf3ca34f9af0c84b25b804c
Ted Lawless <lawlesst@gmail.com>
2014-10-21 12:15:23 -0400

    Add local build scripts.

---
+++ rebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+/etc/init.d/tomcat7 stop
+ant all -Dskiptests=true
+chown -R tomcat7:tomcat7 /var/lib/tomcat7/webapps/rab*
+chown -R tomcat7:tomcat7 /home/vagrant/rab-vitro/data-home/
+/etc/init.d/tomcat7 start
