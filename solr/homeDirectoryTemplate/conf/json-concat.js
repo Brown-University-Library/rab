62364dee213d2707e3b0725152dab3d50d02818a
Steven McCauley <Steven_McCauley@brown.edu>
2017-05-22 14:57:03 -0400

    Add single-valued fields. Typo fixing

--- solr/homeDirectoryTemplate/conf/json-concat.js
+++ solr/homeDirectoryTemplate/conf/json-concat.js
@@ -2,12 +2,12 @@
 // https://lucene.apache.org/solr/4_7_0/solr-core/org/apache/solr/update/processor/StatelessScriptUpdateProcessorFactory.html
 // https://lucene.apache.org/solr/4_2_1/solr-solrj/org/apache/solr/common/SolrInputDocument.html
 
-function fieldLookup(doc, json_txt, field_name, field_alias, last) {
+function fieldLookup(doc, field_name, field_alias, last) {
   last = (typeof last !== 'undefined') ? last : false;
 
-  var vals = doc.getFieldValues(field_name)
+  var vals = doc.getFieldValues(field_name);
 
-  json_txt += '\"' + field_alias + '\":';
+  var json_txt = '\"' + field_alias + '\":';
 
   if ( vals !== null ) {
     json_txt += '[' ;
@@ -28,7 +28,27 @@ function fieldLookup(doc, json_txt, field_name, field_alias, last) {
   }
 
   return json_txt;
-} 
+}
+
+function addSingleValuedField(doc, field_name, field_alias, last ) {
+  last = (typeof last !== 'undefined') ? last : false;
+
+  var val = doc.getFieldValue(field_name);
+
+  var json_txt = '\"' + field_alias + '\":';
+
+  if ( val !== null ) {
+    json_txt += '\"' + val + '\"';
+  } else {
+    json_txt += '\"\"';
+  }
+
+  if ( !(last) ) {
+    json_txt += ',';
+  }
+
+  return json_txt;
+}
 
 function processAdd(cmd) {
   var doc, id, json_txt;
@@ -37,10 +57,15 @@ function processAdd(cmd) {
 
   json_txt = '';
 
-  json_txt += fieldLookup(doc, json_txt, 'affiliations_json', 'affiliations');
-  json_txt += fieldLookup(doc, json_txt, 'contributor_to_json', 'contributor_to');
-  json_txt += fieldLookup(doc, json_txt, 'education_json', 'education');
-  json_txt += fieldLookup(doc, json_txt, 'on_the_web_json', 'on_the_web', last=true);
+  json_txt += addSingleValuedField(doc, 'affiliations_text', 'affiliations_text');
+  json_txt += addSingleValuedField(doc, 'awards', 'awards');
+  json_txt += addSingleValuedField(doc, 'funded_research', 'funded_research');
+  json_txt += addSingleValuedField(doc, 'research_statement', 'research_statement');
+  json_txt += addSingleValuedField(doc, 'email_s', 'email');
+  json_txt += fieldLookup(doc, 'affiliations_json', 'affiliations');
+  json_txt += fieldLookup(doc, 'contributor_to_json', 'contributor_to');
+  json_txt += fieldLookup(doc, 'education_json', 'education');
+  json_txt += fieldLookup(doc, 'on_the_web_json', 'on_the_web', last=true);
 
   doc.addField( 'json_txt', json_txt );
 }
