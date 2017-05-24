93d0ac149c79eae78a087219f1cf5c9a24839bf0
Steven McCauley <Steven_McCauley@brown.edu>
2017-05-24 16:00:51 -0400

    Add credentials, training, appointments

--- solr/homeDirectoryTemplate/conf/json-concat.js
+++ solr/homeDirectoryTemplate/conf/json-concat.js
@@ -67,17 +67,27 @@ function processAdd(cmd) {
 
   json_txt += addSingleValuedField(doc, 'affiliations_text', 'affiliations_text');
   json_txt += addSingleValuedField(doc, 'awards', 'awards');
-  json_txt += addSingleValuedField(doc, 'funded_research', 'funded_research');
-  json_txt += addSingleValuedField(doc, 'research_statement', 'research_statement');
-  json_txt += addSingleValuedField(doc, 'email_s', 'email');
-  json_txt += addSingleValuedField(doc, 'overview_t', 'overview');
   json_txt += addSingleValuedField(doc, 'department_t', 'org_label');
+  json_txt += addSingleValuedField(doc, 'email_s', 'email');
+  json_txt += addSingleValuedField(doc, 'funded_research', 'funded_research');
+  json_txt += addSingleValuedField(doc, 'overview_t', 'overview');
+  json_txt += addSingleValuedField(doc, 'research_overview', 'research_overview');
+  json_txt += addSingleValuedField(doc, 'research_statement', 'research_statement');
+  json_txt += addSingleValuedField(doc, 'scholarly_work', 'scholarly_work');
+  json_txt += addSingleValuedField(doc, 'teaching_overview', 'teaching_overview');
   json_txt += addSingleValuedField(doc, 'title_t', 'title');
+
   json_txt += addMultiValuedField(doc, 'published_in', 'published_in', true);
   json_txt += addMultiValuedField(doc, 'research_areas', 'research_areas', true);
+  json_txt += addMultiValuedField(doc, 'teacher_for', 'teacher_for', true);
+
   json_txt += addMultiValuedField(doc, 'affiliations_json', 'affiliations');
+  json_txt += addMultiValuedField(doc, 'collaborators_json', 'collaborators');
   json_txt += addMultiValuedField(doc, 'contributor_to_json', 'contributor_to');
   json_txt += addMultiValuedField(doc, 'education_json', 'education');
+  json_txt += addMultiValuedField(doc, 'appointments_json', 'appointments');
+  json_txt += addMultiValuedField(doc, 'credentials_json', 'credentials');
+  json_txt += addMultiValuedField(doc, 'training_json', 'training');
   json_txt += addMultiValuedField(doc, 'on_the_web_json', 'on_the_web', false, true);
 
   doc.addField( 'json_txt', json_txt );
